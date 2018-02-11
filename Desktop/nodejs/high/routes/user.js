67f14d56b7df0c8aa4b3e67e2ad9c85ac705854f
wjdghks158 <wjdghks158@naver.com>
2018-02-11 21:13:34 +0900

    make it better

---
+++ Desktop/nodejs/high/routes/user.js
@@ -0,0 +1,180 @@
+/*
+ * 사용자 조회를 담당하는 라우팅 함수 정의
+ *
+ * @date 2016-11-10
+ * @author Mike *
+ */
+
+
+//test용
+var test = function(req, res) {
+	console.log('user 모듈 안에 있는 test 호출됨.');
+
+	// 데이터베이스 객체 참조
+	var database = req.app.get('database');
+    
+    // 데이터베이스 객체가 초기화된 경우, 모델 객체의 findAll 메소드 호출
+	if (database.db) {
+		// 1. 모든 사용자 검색
+		database.UserModel.findAll(function(err, results) {
+			// 에러 발생 시, 클라이언트로 에러 전송
+			if (err) {
+                console.error('사용자 리스트 조회 중 에러 발생 : ' + err.stack);
+                
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 리스트 조회 중 에러 발생</h2>');
+                res.write('<p>' + err.stack + '</p>');
+				res.end();
+                
+                return;
+            }
+
+			if (results) {
+				//console.dir(results);
+                var test_json =  { "나이" : "많임", "안뇽" : "그래안뇽", "age" : "1"};
+
+				res.writeHead('200', {'Content-Type':'application/json;charset=utf8'});
+				res.write(JSON.stringify(test_json));
+				res.end();
+				
+			} else {
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 리스트 조회  실패</h2>');
+				res.end();
+			}
+		});
+	} else {
+		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.write('<h2>데이터베이스 연결 실패</h2>');
+		res.end();
+	}
+	
+};
+
+
+var listuser = function(req, res) {
+	console.log('user 모듈 안에 있는 listuser 호출됨.');
+
+	// 데이터베이스 객체 참조
+	var database = req.app.get('database');
+    
+    // 데이터베이스 객체가 초기화된 경우, 모델 객체의 findAll 메소드 호출
+	if (database.db) {
+		// 1. 모든 사용자 검색
+		database.UserModel.findAll(function(err, results) {
+			// 에러 발생 시, 클라이언트로 에러 전송
+			if (err) {
+                console.error('사용자 리스트 조회 중 에러 발생 : ' + err.stack);
+                
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 리스트 조회 중 에러 발생</h2>');
+                res.write('<p>' + err.stack + '</p>');
+				res.end();
+                
+                return;
+            }
+
+			if (results) {
+				//console.dir(results);
+                console.log("성공");
+				res.writeHead('200', {'Content-Type':'application/json;charset=utf8'});
+				res.write(JSON.stringify(results));
+				res.end();
+				
+			} else {
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 리스트 조회  실패</h2>');
+				res.end();
+			}
+		});
+	} else {
+		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.write('<h2>데이터베이스 연결 실패</h2>');
+		res.end();
+	}
+	
+};
+
+
+var adduser = function(req, res) {
+	console.log('user(user.js) 모듈 안에 있는 adduser 호출됨.');
+
+	var paramEmail = req.body.email || req.query.email;
+    var paramPassword = req.body.password || req.query.password;
+    var paramName = req.body.name || req.query.name;
+	
+    console.log('요청 파라미터 : ' + paramEmail + ', ' + paramPassword + ', ' + paramName);
+    
+    // 데이터베이스 객체 참조
+	var database = req.app.get('database');
+	
+    // 데이터베이스 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
+	if (database.db) {
+		addUser(database, paramEmail, paramPassword, paramName, function(err, addedUser) {
+            // 동일한 id로 추가하려는 경우 에러 발생 - 클라이언트로 에러 전송
+			if (err) {
+                console.error('사용자 추가 중 에러 발생 : ' + err.stack);
+                
+                res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 추가 중 에러 발생</h2>');
+                res.write('<p>' + err.stack + '</p>');
+				res.end();
+                
+                return;
+            }
+			
+            // 결과 객체 있으면 성공 응답 전송
+			if (addedUser) {
+				console.dir(addedUser);
+ 
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				
+				// 뷰 템플레이트를 이용하여 렌더링한 후 전송
+				var context = {title:'사용자 추가 성공'};
+				req.app.render('adduser', context, function(err, html) {
+					if (err) {throw err;}
+					
+					console.log("rendered : " + html);
+					
+					res.end(html);
+				});
+				
+			} else {  // 결과 객체가 없으면 실패 응답 전송
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 추가  실패</h2>');
+				res.end();
+			}
+		});
+	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
+		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.write('<h2>데이터베이스 연결 실패</h2>');
+		res.end();
+	}
+	
+};
+
+
+//사용자를 등록하는 함수
+var addUser = function(database, email, password, name, callback) {
+	console.log('addUser 호출됨 : ' + email + ', ' + password  + ', ' + name);
+	
+	// UserModel 인스턴스 생성
+	var user = new database.UserModel({email:'wjdghks158@naver.com', password:'158774', name:'박정환'});
+    //var user = new database.UserModel({email:email, password:password, name:name});
+    
+	// save()로 저장
+	user.save(function(err) {
+		if (err) {
+			callback(err, null);
+			return;
+		}
+		
+	    console.log("사용자 데이터 추가함.");
+	    callback(null, user);
+	     
+	});
+}
+
+module.exports.test = test;
+module.exports.listuser = listuser;
+module.exports.adduser = adduser;
